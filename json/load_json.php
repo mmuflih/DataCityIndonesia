1b7aabe454dbced20a4a4b6758c3adebe65b2175
muflih <muflic.24@gmail.com>
2015-05-26 07:42:18 +0700

    Json, dump

--- json/load_json.php
+++ json/load_json.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Created by M. Muflikh Kholidin, S.Kom
  * Scala and Web Developer
@@ -8,18 +9,27 @@
  * https://github.com/mufl1c
  * https://bitbucket.org/mufl1c
  */
+
+$json = file_get_contents("DataCities.json");
 $json_decode = json_decode($json);
 $provinces = array();
 $cities = array();
 foreach ($json_decode as $pkey => $province) {
     $provinces[$pkey] = array(
-        'provinceince' => key($province),
+        'province' => key($province),
     );
     foreach ($province as $c) {
         foreach ($c as $ckey => $city) {
-            $cities[$key] = array(
+            $cities[$ckey] = array(
                 'city' => $city
             );
         }
     }
 }
+
+// mysql_connect("localhost", "root", "kholid.in") or die("Error Connection");
+// if(mysql_select_db("master")){
+//     foreach ($cities as $id => $city){
+//         mysql_query("INSERT INTO city (city_id, name) VALUES ('$id', '$city[city]')");
+//     }
+// }
